dff8e3554147fbf9274796e577196871737fbec2
noella.michiels <noella.michiels@hvw-capac.fgov.be>
2023-06-13 08:29:35 +0200

    add project template

--- project-template/src/app/app.module.ts
+++ project-template/src/app/app.module.ts
@@ -1,16 +1,14 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
